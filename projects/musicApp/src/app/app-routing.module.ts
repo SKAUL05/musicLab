919742af4d603cee801d4dbcf7856caf08fb81ac
Sarath Kaul <sarath.kaul@searce.com>
2019-12-30 09:54:35 +0530

    Navbar Component Added

--- projects/musicApp/src/app/app-routing.module.ts
+++ projects/musicApp/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {'path':'music',loadChildren: () => import('./modules/music-directory/music-directory.module').then(m => m.MusicDirectoryModule)},
+  {'path':'playlist',loadChildren: () => import('./modules/playlist/playlist.module').then(m => m.PlaylistModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/musicApp/src/app/app.module.ts
+++ projects/musicApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
