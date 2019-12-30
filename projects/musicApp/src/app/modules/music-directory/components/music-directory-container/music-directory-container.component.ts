a381d503c478e71df42135b005c965f947dca94f
Sarath Kaul <sarath.kaul@searce.com>
2019-12-30 11:21:42 +0530

    Toggle SideNav Added

--- projects/musicApp/src/app/modules/music-directory/components/music-directory-container/music-directory-container.component.ts
+++ projects/musicApp/src/app/modules/music-directory/components/music-directory-container/music-directory-container.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MusicDirectoryContainerComponent implements OnInit {
 
+  showFiller = false;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
