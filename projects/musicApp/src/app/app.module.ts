9905428dc7e8ead5affead72d8225f496cf92907
Sarath Kaul <sarath.kaul@searce.com>
2019-12-30 10:33:27 +0530

    Material Module Ready

--- projects/musicApp/src/app/app.module.ts
+++ projects/musicApp/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MaterialModule} from './modules/material/material.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -19,9 +17,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule
-    ],
+    MaterialModule
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
